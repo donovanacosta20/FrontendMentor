f9b7a5fb89e939954e636378ab69b92efef241c9
Donovan Acosta <dnvnacosta81@outlook.com>
2025-02-21 11:19:31 -0300

    refactoring structure and code

---
+++ FrontendMentor.Core/agecounter/Application/CalculateAge.cs
@@ -0,0 +1,47 @@
+﻿namespace FrontendMentor.Core.agecounter.Application;
+
+public static class CalculateAge
+{
+	public static DateTime Today { get; private set; } = DateTime.Now;
+	public static DateTime Birth { get; set; }
+	public static int CheckDays()
+	{
+		if (Birth.Day == Today.Day && Birth.Month == Today.Month)
+		{
+			return 0;
+		}
+
+		const int daysOfMonth = 31;
+
+		if (Birth.Day > Today.Day && (Birth.Month >= Today.Month || Birth.Month < Today.Month))
+		{
+			return daysOfMonth - (Birth.Day - Today.Day);
+		}
+
+		return Today.Day - Birth.Day;
+	}
+
+	public static int CheckMonths()
+	{
+		const int monthsOfYear = 12;
+
+		if (Birth.Day > Today.Day)
+		{
+			return (Today.Month - Birth.Month - 1 + monthsOfYear) % monthsOfYear;
+		}
+
+		return (Today.Month - Birth.Month + monthsOfYear) % monthsOfYear;
+
+	}
+
+	public static int CheckYears()
+	{
+		if (Today.Month > Birth.Month || (Today.Month == Birth.Month && Today.Day >= Birth.Day))
+		{
+			return (Today.Year - Birth.Year);
+		}
+
+
+		return (Today.Year - Birth.Year) - 1;
+	}
+}
---
+++ FrontendMentor.Core/agecounter/Model/Exception/YearValidator.cs
@@ -0,0 +1,39 @@
+﻿namespace FrontendMentor.Core.agecounter.Model.Exception;
+
+public class YearValidator
+{
+	private readonly DateInfo _dateInfo;
+	private readonly DateError _dateError;
+
+	public YearValidator(DateInfo dateInfo, DateError dateError)
+	{
+		_dateInfo = dateInfo;
+		_dateError = dateError;
+	}
+	public void ValidateYear(DateTime today)
+	{
+		if (string.IsNullOrEmpty(_dateInfo.Year))
+			return;
+
+		if (!int.TryParse(_dateInfo.Year, out int flagYear) || flagYear > today.Year)
+		{
+			_dateError.ErrorYear = "Must be in the past";
+		}
+		else
+		{
+			_dateError.ErrorYear = string.Empty;
+		}
+	}
+
+	public void ValidateYear(DateTime today, DateTime birth)
+	{
+		if (birth > today)
+		{
+			_dateError.ErrorYear = "Must be in the past";
+		}
+		else
+		{
+			_dateError.ErrorYear = string.Empty;
+		}
+	}
+}
